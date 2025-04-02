4d1af85e482dbfefecc69d3dc4deb3ec84575af0
Sunny Kumar <sk7522422@gmail.com>
2025-04-02 15:37:52 +0530

    update dotenv

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/connectMongo');
+const path = require('path');
 
 dotenv.config({ path: './config/config.env' });
 const PORT = process.env.PORT || 5000;
